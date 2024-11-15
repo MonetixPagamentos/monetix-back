b11f8488db812ba78fa61cff9e2945f6b93e5a01
@rjjunior <rogeriojungjunior@gmail.com>
2024-11-15 17:48:58 -0300

    alterado ca-sandbox

--- db/connection.js
+++ db/connection.js
@@ -10,6 +10,7 @@ const sequelize = new Sequelize('monetix', 'root', null, {
 });*/
 
 //prod
+/*
 const sequelize = new Sequelize('defaultdb', 'doadmin', 'AVNS_EZN9VXNMVHMh08FZ-CS', {
   host: 'monetixdb-do-user-18316313-0.i.db.ondigitalocean.com',
   dialect: 'mysql',
@@ -21,21 +22,20 @@ const sequelize = new Sequelize('defaultdb', 'doadmin', 'AVNS_EZN9VXNMVHMh08FZ-C
       ca: fs.readFileSync(path.join(__dirname, 'ca-monetix-db.crt'))
     }
   }
-});
+});*/
 
 //sandbox
-/*
-const sequelize = new Sequelize('defaultdb', 'doadmin', 'AVNS_JtZt-Sw4_6-6GO5U4Ln', {
-  host: 'monetix-sandbox-do-user-18316313-0.k.db.ondigitalocean.com',
+const sequelize = new Sequelize('defaultdb', 'doadmin', 'AVNS_X5grNxqGgswGOQFZqqQ ', {
+  host: 'db-mysql-nyc1-39828-do-user-18325952-0.f.db.ondigitalocean.com',
   dialect: 'mysql',
   port: 25060,
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
-      ca: fs.readFileSync(path.join(__dirname, 'ca-certificate-sandbox.crt'))
+      ca: fs.readFileSync(path.join(__dirname, 'ca-sandbox.crt'))
     }
   }
-});*/
+});
 
 module.exports = sequelize;
