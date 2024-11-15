46fab5b38523f8e1f0be7ea567c162b400023036
Bona <164968826+repnet@users.noreply.github.com>
2024-11-15 19:42:48 -0300

    email

--- routes/email.js
+++ routes/email.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const enviarEmail = require('../components/email');
 const User = require('../db/models/user');
+require('dotenv').config();
+
 
 router.get('/send-email/:to/:subject/:text', async (req, res) => {
 
     const { to, subject, text } = req.params;
 
-    const textEmail = `https://api.monetixpagamentos.com/email/ativacao-user/${text}`
+    const textEmail = process.env.API_BASE_URL +`/email/ativacao-user/${text}`
     try {
         await enviarEmail(to, subject, textEmail);
         return res.status(200).json({ message: 'Email sent successfully' });
@@ -19,7 +21,7 @@ router.get('/send-email/:to/:subject/:text', async (req, res) => {
 router.get('/ativacao-user/:id_user', async (req, res) => {
     const { id_user } = req.params;
     const user = await User.update({status: 1, verificacao_email: 1},{where: { id: id_user, verificacao_email: 0 }});   
-    res.redirect('https://app.monetixpagamentos.com/');       
+    res.redirect(process.env.API_BASE_URL);       
 });
 
 module.exports = router;
