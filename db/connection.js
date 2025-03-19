b770c89405ef183e1a2785585a99d808db6b8949
Bona <gianbona15@gmail.com>
2025-03-19 10:40:56 -0300

    reset database

--- db/connection.js
+++ db/connection.js
@@ -11,7 +11,7 @@ if (process.env.HOMOLOG == 0) { //homolog
     timezone: '-03:00'
   });
 } else if (process.env.HOMOLOG == 1) { // sandobox
-  sequelize = new Sequelize('defaultdb', 'doadmin', 'AVNS_X5grNxqGgswGOQFZqqQ', {
+  sequelize = new Sequelize('defaultdb', 'doadmin', 'AVNS_cm2jJR7gRGZnaPdg_nN', {
     host: 'db-mysql-nyc1-39828-do-user-18325952-0.f.db.ondigitalocean.com',
     dialect: 'mysql',
     port: 25060,
@@ -19,21 +19,21 @@ if (process.env.HOMOLOG == 0) { //homolog
       ssl: {
         require: true,
         rejectUnauthorized: false,
-        ca: fs.readFileSync(path.join(__dirname, 'ca-certificate-sandbox.crt'))
+        ca: fs.readFileSync(path.join(__dirname, 'ca-cortex.crt'))
       }
     },
       timezone: '-03:00'
   });
-} else if (process.env.HOMOLOG == 2) { // sandobox
-  sequelize = new Sequelize('monetix', 'doadmin', 'AVNS_X5grNxqGgswGOQFZqqQ', {
-    host: 'db-mysql-nyc1-39828-do-user-18325952-0.f.db.ondigitalocean.com',
+} else if (process.env.HOMOLOG == 2) { // prod
+  sequelize = new Sequelize('monetix', 'doadmin', 'AVNS_cm2jJR7gRGZnaPdg_nN', {
+    host: 'db-mysql-nyc3-07394-do-user-18325952-0.e.db.ondigitalocean.com',
     dialect: 'mysql',
     port: 25060,
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
-        ca: fs.readFileSync(path.join(__dirname, 'ca-certificate-sandbox.crt'))
+        ca: fs.readFileSync(path.join(__dirname, 'ca-cortex.crt'))
       }
     },
       timezone: '-03:00'
